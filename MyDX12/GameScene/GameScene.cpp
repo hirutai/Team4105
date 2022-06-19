0f409382014a6c04240fcd451ac12fb7f85435cb
R_Yoshioka <AUOdnst1230@outlook.jp>
2022-06-19 17:12:39 +0900

    シーンの切り替えを追加(まだ一部)

--- MyDX12/GameScene/GameScene.cpp
+++ MyDX12/GameScene/GameScene.cpp
@@ -15,6 +15,7 @@
 #include "../Tool/Messenger.h"
 #include "../Tool/DebugJISText.h"
 #include "../GameObject/UnitManager.h"
+#include "../Tool/Easing.h"
 #include <Xinput.h>
 
 #pragma comment(lib, "xinput.lib")
@@ -32,6 +33,9 @@ XIIlib::GameScene::GameScene()
 
 XIIlib::GameScene::~GameScene()
 {
+	for (auto x : screen_curtain) {
+		delete x;
+	}
 	delete gamePad;
 	delete audio;
 }
@@ -49,6 +53,30 @@ void XIIlib::GameScene::Initialize()
 {
 	// Audioの初期化
 	audio = new Audio();
+
+	// 前景テクスチャの初期化
+	// サイズを設定
+	const unsigned int tex_size = 128;
+	const unsigned int s_y = 768 / tex_size;
+	const unsigned int s_x = 1280 / tex_size;
+	m_curtain.resize(s_y);
+
+	for (int i = 0; i < s_y; i++) {
+		m_curtain.at(i).resize(s_x);
+	}
+	// テクスチャも含めて初期化
+	screen_curtain.resize(s_y * s_x);
+	time_curtain.resize(s_y * s_x);
+	for (int i = 0; i < s_y; ++i)
+	{
+		for (int j = 0; j < s_x; ++j) {
+			m_curtain[i][j] = 0;
+			screen_curtain[j + (i * s_x)] =
+				Sprite::Create(4, { (float)(j * tex_size) + (float)(tex_size / 2.0f),(float)(i * tex_size) + (float)(tex_size / 2.0f) }, { 1,1,1,1 }, { 0.5f,0.5f });
+			screen_curtain[j + (i * s_x)]->SetSize({ 0.0f,0.0f });
+			time_curtain[j + (i * s_x)] = 0.0f;
+		}
+	}
 	
 	// シーンの初期化
 	state->Initialize(this);
@@ -90,11 +118,113 @@ void XIIlib::GameScene::DrawSprite()
 {
 	// シーンのテクスチャ描画
 	state->DrawTex();
+	for (auto i : screen_curtain) {
+		i->Draw();
+	}
 }
 
 void XIIlib::GameScene::DrawBackground()
 {
 	state->DrawBackground();
+}
+
+bool XIIlib::GameScene::DrawScreen(bool on_curtain)
+{
+	const unsigned int tex_size = 128;
+	const unsigned int s_y = 768 / tex_size;
+	const unsigned int s_x = 1280 / tex_size;
+	// クローズ
+	if (!on_curtain) {
+		struct Index2 {
+			int y, x;// 要素数の格納用構造体
+		};
+		std::vector<Index2> indecis;
+
+		for (int i = 0; i < s_y; ++i)
+		{
+			for (int j = 0; j < s_x; ++j) {
+				if (m_curtain[i][j] == 0) {
+					indecis.push_back({ i,j });
+				}
+			}
+		}
+
+		int arraysize = indecis.size();
+		if (arraysize != 0) {
+			Index2 ind2 = indecis[rand() % arraysize];
+			m_curtain[ind2.y][ind2.x] = 1;
+			screen_curtain[ind2.x + (ind2.y * s_x)]->SetColor(1, 1, 1, 1);
+		}
+
+		for (int i = 0; i < s_y; ++i) {
+			for (int j = 0; j < s_x; ++j) {
+				if (m_curtain[i][j] == 1)
+				{
+					float newSize = (float)Easing::EaseOutBack((double)time_curtain[j + (i * s_x)], 0, (double)tex_size, 0.5);
+					time_curtain[j + (i * s_x)] += (1.0f / 60.0f);
+					screen_curtain[j + (i * s_x)]->SetSize({ newSize ,newSize });
+					if (time_curtain[j + (i * s_x)] >= 0.5f) {
+						time_curtain[j + (i * s_x)] = 0.0f;
+						m_curtain[i][j] = 2;
+					}
+				}
+			}
+		}
+
+		for (auto y : m_curtain) {
+			for (auto x : y) {
+				if (x == 0 || x == 1)return 0;
+			}
+		}
+		return 1;
+	}
+	// オープン
+	else {
+		struct Index2 {
+			int y, x;// 要素数の格納用構造体
+		};
+		std::vector<Index2> indecis;
+
+		for (int i = 0; i < s_y; ++i)
+		{
+			for (int j = 0; j < s_x; ++j) {
+				if (m_curtain[i][j] == 2) {
+					indecis.push_back({ i,j });
+				}
+			}
+		}
+
+		int arraysize = indecis.size();
+		if (arraysize != 0) {
+			Index2 ind2 = indecis[rand() % arraysize];
+			m_curtain[ind2.y][ind2.x] = 1;
+		}
+
+		for (int i = 0; i < s_y; ++i) {
+			for (int j = 0; j < s_x; ++j) {
+				if (m_curtain[i][j] == 1)
+				{
+					float newSize = (float)Easing::EaseInBack((double)time_curtain[j + (i * s_x)], 0, (double)tex_size, 0.5);
+					time_curtain[j + (i * s_x)] += (1.0f / 60.0f);
+					float result = (float)tex_size - newSize;
+					screen_curtain[j + (i * s_x)]->SetSize({ result ,result });
+					if (time_curtain[j + (i * s_x)] >= 0.5f) {
+						time_curtain[j + (i * s_x)] = 0.0f;
+						screen_curtain[j + (i * s_x)]->SetColor(0, 0, 0, 0);
+						m_curtain[i][j] = 0;
+					}
+				}
+			}
+		}
+
+		for (auto y : m_curtain) {
+			for (auto x : y) {
+				if (x == 1 || x == 2)return 0;
+			}
+		}
+
+		return 1;
+	}
 }
 
 XIIlib::Audio* XIIlib::GameScene::GetAudio() const
