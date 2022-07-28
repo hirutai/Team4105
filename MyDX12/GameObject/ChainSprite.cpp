43698f46cee7648c414d719bba61989574a53fe8
R_Yoshioka <AUOdnst1230@outlook.jp>
2022-07-28 18:25:04 +0900

    文字複数の追加

---
+++ MyDX12/GameObject/ChainSprite.cpp
@@ -0,0 +1,48 @@
+#include "ChainSprite.h"
+#include "TexMoveAction.h"
+
+XIIlib::ChainSprite* XIIlib::ChainSprite::Create()
+{
+	ChainSprite* pChainSprite = new ChainSprite();
+
+	if (pChainSprite == nullptr) {
+		return nullptr;
+	}
+}
+
+XIIlib::ChainSprite::ChainSprite()
+{
+	pTexs.reserve(128);
+	activeNum = 0;
+}
+
+XIIlib::ChainSprite::~ChainSprite()
+{
+	if (pTexs.size() != 0) {
+		for (auto tex : pTexs) {
+			delete tex;
+		}
+
+		pTexs.clear();
+	}
+}
+
+void XIIlib::ChainSprite::Update()
+{
+	if (pTexs.size() == 0 || pTexs.size() == activeNum)return;
+
+	pTexs[activeNum]->Update();
+	if (pTexs[activeNum]->GetFinish())activeNum++;
+}
+
+void XIIlib::ChainSprite::Draw()
+{
+	for (auto tex : pTexs) {
+		tex->Draw();
+	}
+}
+
+void XIIlib::ChainSprite::AddMoji(float posX, float posY, int texNum)
+{
+	pTexs.push_back(TexMoveAction::Create(posX,posY,texNum));
+}
---
+++ MyDX12/GameObject/ChainSprite.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+
+namespace XIIlib{
+	class TexMoveAction;
+
+	class ChainSprite {
+	private:
+		std::vector<TexMoveAction*> pTexs;
+
+		int activeNum = 0;
+	public:
+		static ChainSprite* Create();
+
+		ChainSprite();
+		~ChainSprite();
+
+		void Update();
+
+		void Draw();
+
+		void AddMoji(float posX, float posY, int texNum);
+	};
+}
