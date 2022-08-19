f2e4dc4475b26feeaf8d27b19a83200a0424c57a
Ishi-so <k019g1077@g.neec.ac.jp>
2022-08-19 21:37:33 +0900

    クラスの整理

--- MyDX12/GameObject/Boss.h
+++ MyDX12/GameObject/Boss.h
@@ -15,56 +15,48 @@ namespace XIIlib {
 	};
 	class Boss : public Unit {
 	private:
+		const int DISPLAY_FRAME = 12;
+		static const int MAX_TILE = 8;
+		static const int METEORS_MAX = 15;
+	private: // メンバ変数
 		Math::Point2 point_attack;
 		AREA type_attack = AREA::NONE;
 		Object3D* object3d2 = nullptr;
+		BossState bossState = BossState::wait;
+		BossType bossType = BossType::normal;
 		
 		int count = 0;
-		const int METEORS_MAX = 15;
-		int numbersA[15];
-		int numbersB[15];
-	public:
-		Boss();
-		~Boss();
-		// 生成関数
-		static std::shared_ptr<Boss> Create(int point_x, int point_z);
+		int numbersA[METEORS_MAX];
+		int numbersB[METEORS_MAX];
+		int switchingCount = 0;
+		bool harfHpFlag = false;
+		bool switching = false;//画像切り替えよう
+		bool tileDeth[MAX_TILE] = {};
+		int tileNum = 0;
 
+	public: // 継承クラス
 		void Initialize()override;
 		void Update()override;
 		void Draw()override;
-
 		void Action()override;
 		void Attack()override;
 		void Target();
 		void Move()override;
 		bool AttackAreaExists()override;
-		void AttackAreaDraw();
-
-		bool switching = false;//画像切り替えよう
-		int switchingCount = 0;
-
-		bool harfHpFlag = false;
-		BossState bossState = BossState::wait;
-		BossType bossType = BossType::normal;
-
 		void IniState()override;
-
 		void SetHitDamage(float attackPoint)override;				// ダメージを受ける関数
-
 		void CreateAttackArea()override;
 
-		Math::Point2 GetElementStock()const { return element_stock; }
+	public: // メンバ関数
+		Boss();
+		~Boss();
+		// 生成関数
+		static std::shared_ptr<Boss> Create(int point_x, int point_z);
+		void AttackAreaDraw();
 		bool MoveAreaCheck(Math::Point2 crPos, Math::Point2 vec, int tileNum)override;
-
 		void InitAttackDisplay();
-
 		bool CheckMeteorArea(Math::Point2 meteorPos);
-	private:
-		// 変数
-		bool tileDeth[8] = {};
-		int tileNum = 0;
-		const int DISPLAY_FRAME = 12;
-		const int MAX_TILE = 8;
+		Math::Point2 GetElementStock()const { return element_stock; }
 	};
 }
 
